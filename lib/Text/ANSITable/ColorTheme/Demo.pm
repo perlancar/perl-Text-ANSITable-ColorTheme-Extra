4329d24900544578a7bede7102760d09cfce8008
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2013-04-28 08:14:34 +0700

    Allow setting bgcolors

--- lib/Text/ANSITable/ColorTheme/Demo.pm
+++ lib/Text/ANSITable/ColorTheme/Demo.pm
@@ -17,13 +17,23 @@ our %color_themes = (
 Accept arguments C<border1> and C<border2> to set range of random colors.
 Otherwise full range (000000-ffffff) is used.
 
+Also accepts C<border1_bg> and C<border2_bg> to set random background RGB
+colors.
+
 _
         colors => {
             border => sub {
                 my ($self, %args) = @_;
-                my $rgb1 = $self->{color_theme_args}{border1};
-                my $rgb2 = $self->{color_theme_args}{border2};
-                rand_rgb_color($rgb1, $rgb2);
+                my $rgbf1 = $self->{color_theme_args}{border1};
+                my $rgbf2 = $self->{color_theme_args}{border2};
+                my $rgbf = rand_rgb_color($rgbf1, $rgbf2);
+                my $rgbb1 = $self->{color_theme_args}{border1_bg};
+                my $rgbb2 = $self->{color_theme_args}{border2_bg};
+                my $rgbb;
+                if ($rgbb1 && $rgbb2) {
+                    $rgbb = rand_rgb_color($rgbb1, $rgbb2);
+                }
+                [$rgbf, $rgbb];
             },
         },
     },
@@ -32,15 +42,16 @@ _
         summary => 'Demoes coderef in item color',
         description => <<'_',
 
-Accept arguments C<border1> and C<border2> to set first and second RGB colors.
+Accept arguments C<border1> and C<border2> to set first (top) and second
+(bottom) foreground RGB colors. Colors will fade from the top color to bottom
+color.
+
+Also accepts C<border1_bg> and C<border2_bg> to set background RGB colors.
 
 _
         colors => {
             border => sub {
                 my ($self, %args) = @_;
-
-                my $rgb1 = $self->{color_theme_args}{border1} // 'ffffff';
-                my $rgb2 = $self->{color_theme_args}{border2} // '444444';
 
                 my $y;
                 my $num_rows = @{$self->{_draw}{frows}};
@@ -57,9 +68,19 @@ _
                     $y = $args{row_idx}+3;
                 }
 
-                my $rgb = mix_2_rgb_colors($rgb1, $rgb2, $y/($num_rows+3));
-                #say "D:$rgb";
-                $rgb;
+                my $rgbf1 = $self->{color_theme_args}{border1} // 'ffffff';
+                my $rgbf2 = $self->{color_theme_args}{border2} // '444444';
+                my $rgbf = mix_2_rgb_colors($rgbf1, $rgbf2, $y/($num_rows+3));
+
+                my $rgbb1 = $self->{color_theme_args}{border1_bg};
+                my $rgbb2 = $self->{color_theme_args}{border2_bg};
+                my $rgbb;
+                if ($rgbb1 && $rgbb2) {
+                    $rgbb = mix_2_rgb_colors($rgbb1, $rgbb2, $y/($num_rows+3));
+                }
+
+                #say "D:$rgbf, $rgbb";
+                [$rgbf, $rgbb];
             },
         },
     },
