c10be68481b5069e9b703f6ac2b244fff244e3ff
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-05-03 19:02:40 +0700

    Add themes: demo_random_{8,16,256,24bit}

--- lib/Text/ANSITable/ColorTheme/Demo.pm
+++ lib/Text/ANSITable/ColorTheme/Demo.pm
@@ -12,10 +12,13 @@ use Text::ANSITable::ColorTheme::Default ();
 
 our %color_themes = ();
 
-my $ct = clone $Text::ANSITable::ColorTheme::Default::color_themes{default_nogradation};
+my $defct = $Text::ANSITable::ColorTheme::Default::color_themes{default_nogradation};
 
-$ct->{summary} = 'Demoes coderef in item color';
-$ct->{description} = <<'_';
+{
+    my $ct = clone $defct;
+
+    $ct->{summary} = 'Demoes coderef in item color';
+    $ct->{description} = <<'_';
 
 Accept arguments C<border1> and C<border2> to set range of random colors.
 Otherwise full range (000000-ffffff) is used.
@@ -24,21 +27,74 @@ Also accepts C<border1_bg> and C<border2_bg> to set random background RGB
 colors.
 
 _
-$ct->{colors}{border} = sub {
-    my ($self, %args) = @_;
-    my $rgbf1 = $self->{color_theme_args}{border1};
-    my $rgbf2 = $self->{color_theme_args}{border2};
-    my $rgbf = rand_rgb_color($rgbf1, $rgbf2);
-    my $rgbb1 = $self->{color_theme_args}{border1_bg};
-    my $rgbb2 = $self->{color_theme_args}{border2_bg};
-    my $rgbb;
-    if ($rgbb1 && $rgbb2) {
-        $rgbb = rand_rgb_color($rgbb1, $rgbb2);
-    }
-    [$rgbf, $rgbb];
-};
+    $ct->{colors}{border} = sub {
+        my ($self, %args) = @_;
+        my $rgbf1 = $self->{color_theme_args}{border1};
+        my $rgbf2 = $self->{color_theme_args}{border2};
+        my $rgbf = rand_rgb_color($rgbf1, $rgbf2);
+        my $rgbb1 = $self->{color_theme_args}{border1_bg};
+        my $rgbb2 = $self->{color_theme_args}{border2_bg};
+        my $rgbb;
+        if ($rgbb1 && $rgbb2) {
+            $rgbb = rand_rgb_color($rgbb1, $rgbb2);
+        }
+        [$rgbf, $rgbb];
+    };
 
-$color_themes{demo_random_border_color} = $ct;
+    $color_themes{demo_random_border_color} = $ct;
+}
+
+{
+    my $ct = {
+        summary => "Show random 8 color",
+    };
+    my $sub = sub {
+        "\e[".(30+int(rand()*8))."m";
+    };
+    for my $c (keys %{ $defct->{colors} }) {
+        $ct->{colors}{$c} = $sub;
+    }
+    $color_themes{demo_random_8} = $ct;
+}
+
+{
+    my $ct = {
+        summary => "Show random 16 color",
+    };
+    my $sub = sub {
+        "\e[".(30+int(rand()*8)).(rand() > 0.5 ? ";1":"")."m";
+    };
+    for my $c (keys %{ $defct->{colors} }) {
+        $ct->{colors}{$c} = $sub;
+    }
+    $color_themes{demo_random_16} = $ct;
+}
+
+{
+    my $ct = {
+        summary => "Show random 256 color",
+    };
+    my $sub = sub {
+        "\e[38;5;".int(rand()*256)."m";
+    };
+    for my $c (keys %{ $defct->{colors} }) {
+        $ct->{colors}{$c} = $sub;
+    }
+    $color_themes{demo_random_256} = $ct;
+}
+
+{
+    my $ct = {
+        summary => "Show random 24bit color",
+    };
+    my $sub = sub {
+        rand_rgb_color();
+    };
+    for my $c (keys %{ $defct->{colors} }) {
+        $ct->{colors}{$c} = $sub;
+    }
+    $color_themes{demo_random_24bit} = $ct;
+}
 
 1;
 # ABSTRACT: Demo color themes
